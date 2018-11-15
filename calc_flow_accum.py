d10916dcf4782311c8d1ca7c4755bbf81d5ac3f3
cmshobe <charles.shobe@colorado.edu>
2018-11-15 11:12:15 -0700

    fixing fn name

--- calc_flow_accum.py
+++ calc_flow_accum.py
@@ -17,7 +17,7 @@ accum_output_raster = 'small_crop_buffer_accum'
 max_memory_usage = 28000 #mb = 28 gb out of 31.2 available
 
 #run r.watershed to calc flow accumulation
-r.terraflow(flags='sm', elevation=input_elev_map,
+r.watershed(flags='sm', elevation=input_elev_map,
             direction=direction_output_raster,
             accumulation=accum_output_raster,
             memory=max_memory_usage)
